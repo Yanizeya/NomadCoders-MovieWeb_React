0f0804022f3b36c5e292a4aacb9783b6365eaf10
Yanizeya <43463709+Yanizeya@users.noreply.github.com>
2021-05-12 23:41:37 +0900

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import Potato from "./Potato";
 
 ReactDOM.render(<App />, document.getElementById("potato"));
+//component 컴포넌트가 데이터를 보여주게함
